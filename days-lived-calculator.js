3164370dfd092b27679cefaf2e7244391c0f6fef
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-05-20 11:24:57 -0600

    add toLocaleString to add comma to number

--- days-lived-calculator.js
+++ days-lived-calculator.js
@@ -2,4 +2,4 @@ var ageInYears = prompt("How old are you in years?");
 var ageInDays = Number(ageInYears) * 365;
 alert("You are approximately " + ageInDays + " days old!");
 document.getElementById("age-in-years").textContent = "You are " + ageInYears + " years old!";
-document.getElementById("age-in-days").textContent = "You are approximately " + ageInDays + " days old!";
+document.getElementById("age-in-days").textContent = "You are approximately " + ageInDays.toLocaleString() + " days old!";
